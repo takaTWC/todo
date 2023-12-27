b10d2a4eeef875df1acb544e6d7cc9a4bebd391f
takaTWC <146624240+takaTWC@users.noreply.github.com>
2023-12-27 21:20:42 +0900

    [Add]UserShow

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
 
+  resources :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
