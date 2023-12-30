214fe3b40869fb93f35a09e281312c59a86ec4f1
takaTWC <146624240+takaTWC@users.noreply.github.com>
2023-12-30 12:21:53 +0900

    [Add]投稿機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
-    
+    @list = List.new
+    @lists = List.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users
+  resources :lists
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
